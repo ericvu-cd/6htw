e150964f30ead1d3c569e97825b50b318e5957ef
eric vu <ericvu64@gmail.com>
2026-09-08 17:08:37 +0800

    R5#2

--- win-screen.js
+++ win-screen.js
@@ -504,7 +504,7 @@ function _makeCloseButton(isPlayer) {
         background:rgba(255,255,255,.06);
         color:rgba(255,255,255,.65);
     `;
-    btnClose.innerHTML = '<span style="filter: brightness(1.4) saturate(1.3);">🔚</span> 關閉，返回平台';
+    btnClose.innerHTML = '<span style="filter: brightness(1.9) saturate(1.8) drop-shadow(0 0 4px rgba(255,255,255,0.9));">🔚</span> 關閉，返回平台';
     btnClose.onclick = () => { if (typeof closeTaskToPlatform === "function") closeTaskToPlatform(); };
     return btnClose;
 }
